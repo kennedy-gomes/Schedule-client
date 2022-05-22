46c651fdb656297037bbfd84079874ed3a3bf820
Kennedy Gomes <gomeskennedy11@gmail.com>
2022-05-22 16:22:58 -0300

    Criando navbar

--- schedule-client/src/app/core/core.module.ts
+++ schedule-client/src/app/core/core.module.ts
@@ -4,11 +4,12 @@ import { CommonModule } from '@angular/common';
 import { LayoutAuthComponent } from './components/layout-auth/page/layout-auth.component';
 import { LayoutNoAuthComponent } from './components/layout-no-auth/layout-no-auth.component';
 import { RouterModule } from '@angular/router';
+import { NavbarComponent } from './components/layout-auth/components/navbar/navbar.component';
 
 
 
 @NgModule({
-  declarations: [LayoutAuthComponent, LayoutNoAuthComponent],
+  declarations: [LayoutAuthComponent, LayoutNoAuthComponent, NavbarComponent],
   imports: [
     CommonModule,
     RouterModule,
